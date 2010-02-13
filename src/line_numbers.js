382fb51b38230d9dc13146509805741125962a20
Andrea Ercolino <cappuccino.e.cornetto@gmail.com>
2010-02-13 21:39:18 +0100

    fix - simplified line numbers generation: better and faster

--- src/line_numbers.js
+++ src/line_numbers.js
@@ -63,130 +63,32 @@
         }
         
         /**
-         * Converts lines inside the given dom_element to list items
+         * Converts lines inside the given dom_element to list items into an
+         * ordered list element
          * 
          * @param {Element} dom_element
          */
         function addLineNumbers( dom_element ) 
         {
             var result = $( dom_element ).html();
-            result = extractImmediateBreaksInsideSpans(result);
-            result = convertLinesInsideSpans(result);
-            result = convertRemainingLines(result);
-            result = fillEmptyListItems(result);
+            var expr = /(.*?)<br>/ig;
+            result = result.replace(expr, function( all, line ) 
+            {
+                return '<li>' + line + '</li>';
+            });
+            expr = /(<span [^>]+>)(.*?)(<\/span>)/ig;
+            result = result.replace(expr, function( all, openSpan, insideSpan, closeSpan ) 
+            {
+                var expr = /<\/li><li>/ig;
+                insideSpan = insideSpan.replace(expr, function( all ) 
+                {
+                    var result = closeSpan + all + openSpan;
+                    return result;
+                });
+                result = openSpan + insideSpan + closeSpan;
+                return result;
+            });
             result = '<ol>' + result + '</ol>';
             dom_element.innerHTML = result;
-        }
-        
-    	/**
-         * Returns a list item that wraps he given textBeforeLastLine or 
-         * lastLine
-         * 
-         * @param {String} lineBeforeLastLine
-         * @param {String} textBeforeLastLine
-         * @param {String} lastLine
-         * @param {String} openSpan
-         * 
-         * @return String
-         */
-        function makeListItem( lineBeforeLastLine, textBeforeLastLine, lastLine, openSpan ) 
-        {
-            var closeSpan = openSpan ? '</span>' : '';
-            var result = '';
-            if (lineBeforeLastLine) 
-            {
-                result = '<li>' + openSpan + textBeforeLastLine + closeSpan + '</li>';
-            }
-            else if (lastLine) 
-            {
-                result = '<li>' + openSpan + lastLine + closeSpan + '</li>';
-            }
-            return result;
-        }
-        
-        /**
-         * Returns the given html, after moving out of the spans their immediate
-         * breaks, if any
-         * 
-         * @param {String} html
-         * 
-         * @return String
-         */
-        function extractImmediateBreaksInsideSpans( html )
-        {
-            var expr = /(<span [^>]+>)((?:(?:&nbsp;|\xA0)<br>)+)(.*?)(<\/span>)/ig;
-            var repl = '$2$1$3$4';
-            var result = html.replace( expr, repl );
-            return result;
-        }
-        
-        /**
-         * Returns the given html, after wrapping each line inside spans with an
-         * <li> element
-         * 
-         * @param {String} html
-         * 
-         * @return String
-         */
-        function convertLinesInsideSpans( html )
-        {
-            var expr = /(.*?)(<span .*?>)(.*?)(?:<\/span>(?:&nbsp;|\xA0)<br>|<\/span>)/ig;
-            var repl = function( all, anythingBeforeSpan, openSpan, anythingInsideSpan ) 
-            {
-                if (! /<br>/i.test(anythingInsideSpan)) 
-                {
-                    return all;
-                }
-                var expr = /((.*?)(?:&nbsp;|\xA0)<br>)|(.*)/ig;
-                var repl = function( all, lineBeforeLastLine, textBeforeLastLine, lastLine ) 
-                {
-                    var result = makeListItem(lineBeforeLastLine, textBeforeLastLine, lastLine, openSpan);
-                    return result;
-                };
-                var result = anythingBeforeSpan + anythingInsideSpan.replace(expr, repl);
-                return result;
-            };
-            var result = html.replace(expr, repl);
-            return result;
-        }
-        
-        /**
-         * Returns the given html, after wrapping each remaining line with an
-         * <li> element
-         * 
-         * @param {String} html
-         * 
-         * @return String
-         */
-        function convertRemainingLines( html ) 
-        {
-            var expr = /(<li>.*?<\/li>)|((.*?)(?:&nbsp;|\xA0)<br>)|(.+)/ig;
-            var repl = function( all, prev_li, lineBeforeLastLine, textBeforeLastLine, lastLine ) 
-            {
-                if (prev_li) 
-                {
-                    return all;
-                }
-                var result = makeListItem(lineBeforeLastLine, textBeforeLastLine, lastLine, '');
-                return result;
-            };
-            var result = html.replace(expr, repl);
-            return result;
-        }
-        
-        /**
-         * Returns the given html, after filling each empty <li> element with a
-         * space replacement
-         * 
-         * @param {String} html
-         * 
-         * @return String
-         */
-        function fillEmptyListItems( html )
-        {
-            var expr = /<li><\/li>/ig;
-            var repl = '<li>' + $.chili.replaceSpace + '</li>';
-            var result = html.replace( expr, repl );
-            return result;
         }
     }
