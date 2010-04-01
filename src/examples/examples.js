52a0a45d124ea1e9bca1982f6cd6adb3648eeb52
Andrea Ercolino <cappuccino.e.cornetto@gmail.com>
2010-04-01 19:44:33 +0200

    fix - properly escape setup code

--- src/examples/examples.js
+++ src/examples/examples.js
@@ -4,25 +4,31 @@
 $( function() {
 //---------------------------------------------------collapsible sections start
 	$( 'pre' ).hide();
-	$( '.section h3' ).css( { cursor: 'pointer' } ).bind( 'click', function() {
-		$( this ).parent().find( 'pre' ).toggle();
-	} );
+	$( '.section h3' ).css({cursor: 'pointer'}).bind('click', function() 
+    {
+		$(this).parent().find('pre').toggle();
+	});
 //---------------------------------------------------collapsible sections end--
 
 //-------------------------------------------------------setup reflection start
 	var script_text = $.browser.msie 
 		? document.scripts['setup'] && document.scripts['setup'].text
-		: $( '#setup' ).text()
+		: $('#setup').text()
 	;
-	if( !script_text ) script_text = '';
-	$( '#show_setup' ).html( ''
-		+'<h4>jQuery version is ' + $.fn.jquery + ' (latest)</h4>'
-		+'<pre><code class="chili-lang-javascript">'
+	if( !script_text ) 
+	{
+	    script_text = '';
+	}
+	script_text = script_text
+        .replace(/&/g, '&amp;')
+        .replace(/</g, '&lt;')
+        .replace(/>/g, '&gt;')
+    ;
+	$( '#show_setup' ).html(''
+		+ '<h4>jQuery version is ' + $.fn.jquery + ' (latest)</h4>'
+		+ '<pre><code class="chili-lang-javascript">'
 			+script_text
-			.replace( '&', '&amp;' )
-			.replace( '<', '&lt;' )
-			.replace( '>', '&gt;' )
-		+'</code></pre>'
+		+ '</code></pre>'
 	);
 	$( '#show_setup > pre > code' ).chili();
 //-------------------------------------------------------setup reflection end--
